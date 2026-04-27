a86e2a3f8abe1b32c2b11f9c7ae162e24e172e26
Anibal Vasquez Lara <anvasquez94@gmail.com>
2026-04-27 11:27:17 -0500

    Mejora del Tag AnadirProducto: Se valida que al menos un producto exista en el carro y se modifica el Then.

--- src/test/java/org/autoweb/questions/MyCartVisible.java
+++ src/test/java/org/autoweb/questions/MyCartVisible.java
@@ -2,6 +2,7 @@ package org.autoweb.questions;
 
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import org.autoweb.ui.InventoryPage;
 import org.autoweb.ui.MyCartPage;
 
@@ -17,5 +18,12 @@ public class MyCartVisible implements Question<Boolean> {
                 MyCartPage.TITLE_MYCART.resolveFor(actor)
                         .getText().equals("Your Cart");
 
+    }
+    // Nueva Question para validar que hay al menos un producto en el carrito
+    public static Question<Boolean> atLeastOneProductInCart() {
+        return actor -> {
+            // Obtiene la lista de elementos y verifica si hay al menos uno
+            return !BrowseTheWeb.as(actor).findAll(MyCartPage.CART_ITEMS).isEmpty();
+        };
     }
 }
